5f77a629fca70be9cd1743f23f5ff0b56c240ba2
ken <ken@whenling.com>
2016-08-02 14:24:45 +0800

    converter

---
+++ castle-framework/castle-repo-jpa/src/main/java/com/whenling/castle/repo/jpa/converter/ListMapToJsonConverter.java
@@ -0,0 +1,55 @@
+package com.whenling.castle.repo.jpa.converter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.SimpleType;
+import com.google.common.base.Strings;
+import com.whenling.castle.core.SpringContext;
+
+@Converter
+public class ListMapToJsonConverter implements AttributeConverter<Object, String> {
+
+	private ObjectMapper objectMapper;
+
+	@Override
+	public String convertToDatabaseColumn(Object attribute) {
+		if (attribute == null) {
+			return null;
+		}
+		try {
+			return getObjectMapper().writeValueAsString(attribute);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public Object convertToEntityAttribute(String dbData) {
+		if (Strings.isNullOrEmpty(dbData)) {
+			return null;
+		}
+		try {
+			return getObjectMapper().readValue(dbData,
+					CollectionType.construct(ArrayList.class, MapType.construct(HashMap.class, SimpleType.construct(String.class), SimpleType.construct(Object.class))));
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public ObjectMapper getObjectMapper() {
+		if (objectMapper == null) {
+			objectMapper = SpringContext.getBean(ObjectMapper.class);
+		}
+		return objectMapper;
+	}
+
+}
---
+++ castle-framework/castle-repo-jpa/src/main/java/com/whenling/castle/repo/jpa/converter/StringArrayConverter.java
@@ -0,0 +1,37 @@
+package com.whenling.castle.repo.jpa.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class StringArrayConverter implements AttributeConverter<Object, String> {
+
+	private static final AttributeConverter<Object, String> target = new ArrayConverter(new StringConverter());
+
+	@Override
+	public String convertToDatabaseColumn(Object attribute) {
+		return target.convertToDatabaseColumn(attribute);
+	}
+
+	@Override
+	public Object convertToEntityAttribute(String dbData) {
+		return target.convertToEntityAttribute(dbData);
+	}
+
+	private static class StringConverter implements AttributeConverter<Object, String> {
+
+		@Override
+		public String convertToDatabaseColumn(Object attribute) {
+			if (attribute == null)
+				return null;
+			return attribute.toString();
+		}
+
+		@Override
+		public Object convertToEntityAttribute(String dbData) {
+			return dbData;
+		}
+
+	}
+
+}
