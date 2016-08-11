b53d7f79f43349b2a2cb7c5b80988df8d1be2512
ken <ken@whenling.com>
2016-08-11 11:01:53 +0800

    jpa common

--- castle-framework/castle-repo-jpa/src/main/java/com/whenling/castle/repo/jpa/BaseJpaRepositoryImpl.java
+++ castle-framework/castle-repo-jpa/src/main/java/com/whenling/castle/repo/jpa/BaseJpaRepositoryImpl.java
@@ -3,21 +3,62 @@ package com.whenling.castle.repo.jpa;
 import java.io.Serializable;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 import org.springframework.data.jpa.repository.support.QueryDslJpaRepository;
 import org.springframework.data.querydsl.EntityPathResolver;
+import org.springframework.util.ClassUtils;
 
-public class BaseJpaRepositoryImpl<T, I extends Serializable> extends QueryDslJpaRepository<T, I>
-		implements BaseJpaRepository<T, I> {
+import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
+
+public class BaseJpaRepositoryImpl<T, I extends Serializable> extends QueryDslJpaRepository<T, I> implements BaseJpaRepository<T, I> {
 
 	public BaseJpaRepositoryImpl(JpaEntityInformation<T, I> entityInformation, EntityManager entityManager) {
 		super(entityInformation, entityManager);
 	}
 
-	public BaseJpaRepositoryImpl(JpaEntityInformation<T, I> entityInformation, EntityManager entityManager,
-			EntityPathResolver resolver) {
+	public BaseJpaRepositoryImpl(JpaEntityInformation<T, I> entityInformation, EntityManager entityManager, EntityPathResolver resolver) {
 		super(entityInformation, entityManager, resolver);
 	}
 
+	@Override
+	protected <S extends T> TypedQuery<S> getQuery(Specification<S> spec, Class<S> domainClass, Sort sort) {
+
+		// 排序实体
+		if (ClassUtils.isAssignable(SortEntity.class, domainClass)) {
+
+			Sort sortNoAsc = new Sort("sortNo");
+			if (sort == null) {
+				sort = sortNoAsc;
+			} else {
+				if (!Iterables.tryFind(sort, s -> {
+					return Objects.equal(s.getProperty(), "sortNo");
+				}).isPresent()) {
+					sort.and(sortNoAsc);
+				}
+
+			}
+		}
+
+		// 数据默认按创建时间倒序排序
+		if (ClassUtils.isAssignable(DataEntity.class, domainClass)) {
+			Sort createdDateDesc = new Sort(Direction.DESC, "createdDate");
+
+			if (sort == null) {
+				sort = createdDateDesc;
+			} else {
+				if (!Iterables.tryFind(sort, s -> {
+					return Objects.equal(s.getProperty(), "createdDate");
+				}).isPresent()) {
+					sort.and(createdDateDesc);
+				}
+			}
+		}
+		return super.getQuery(spec, domainClass, sort);
+	}
 }
