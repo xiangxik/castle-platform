839cffb4a0f088d3c0cf52c7ec6d1a969f28a307
ken <ken@whenling.com>
2016-08-29 22:59:28 +0800

    websocket

---
+++ castle-framework/castle-websocket/src/main/java/com/whenling/castle/websocket/HandshakeHandler.java
@@ -0,0 +1,7 @@
+package com.whenling.castle.websocket;
+
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+
+public class HandshakeHandler extends DefaultHandshakeHandler {
+
+}
---
+++ castle-framework/castle-websocket/src/main/java/com/whenling/castle/websocket/HandshakeInterceptor.java
@@ -0,0 +1,23 @@
+package com.whenling.castle.websocket;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+		System.out.println("before handshake");
+		return super.beforeHandshake(request, response, wsHandler, attributes);
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception ex) {
+		System.out.println("after handshake");
+		super.afterHandshake(request, response, wsHandler, ex);
+	}
+}
