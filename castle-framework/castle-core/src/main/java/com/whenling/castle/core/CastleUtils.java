2edda7699dc978fa9147f3e0d80f89367412980e
kongxiangxi <kongxiangxi@192.168.0.103>
2017-01-12 17:40:41 +0800

    util

---
+++ castle-framework/castle-core/src/main/java/com/whenling/castle/core/CastleUtils.java
@@ -0,0 +1,38 @@
+package com.whenling.castle.core;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.google.common.base.Strings;
+
+public class CastleUtils {
+
+	private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+	public static String formatDate(Date date) {
+		if (date == null) {
+			return "";
+		}
+		return DATE_FORMAT.format(date);
+	}
+
+	public static Date toDate(String dateString) {
+		if (Strings.isNullOrEmpty(dateString)) {
+			return null;
+		}
+		try {
+			return DATE_FORMAT.parse(dateString);
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static String formatDateTime(Date date) {
+		if (date == null) {
+			return "";
+		}
+		return DATE_TIME_FORMAT.format(date);
+	}
+}
